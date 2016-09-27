c820be0c59f8ccf3d1fb50c502078636d6bd900c
Lachlan Donald <lachlan@ljd.cc>
2016-09-27 12:58:42 +1000

    Prevent env state from bleeding from add test

--- add_test.go
+++ add_test.go
@@ -8,6 +8,11 @@ func ExampleAddCommand() {
 	os.Setenv("AWS_VAULT_BACKEND", "file")
 	os.Setenv("AWS_VAULT_FILE_PASSPHRASE", "password")
 
+	defer os.Unsetenv("AWS_ACCESS_KEY_ID")
+	defer os.Unsetenv("AWS_SECRET_ACCESS_KEY")
+	defer os.Unsetenv("AWS_VAULT_BACKEND")
+	defer os.Unsetenv("AWS_VAULT_FILE_PASSPHRASE")
+
 	run([]string{"add", "--env", "foo"}, os.Exit)
 	// Output:
 	// Added credentials to profile "foo" in vault

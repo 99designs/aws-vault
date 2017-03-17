1e00ea22c26e0dfb9f2f0b81ffe1bceb085801e7
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2017-03-17 13:07:03 +0100

    Updated linux command to use ip

--- alias_linux.go
+++ alias_linux.go
@@ -5,5 +5,5 @@ package main
 import "os/exec"
 
 func installNetworkAlias() ([]byte, error) {
-	return exec.Command("ifconfig", "lo:0", "169.254.169.254").CombinedOutput()
+	return exec.Command("ip", "addr", "add", "169.254.169.254/24", "dev", "lo", "label", "lo:0").CombinedOutput()
 }

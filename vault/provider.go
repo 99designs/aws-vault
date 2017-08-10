b709ddd974f372e697f2a40655fbb5832e46c0fa
Lachlan Donald <lachlan@ljd.cc>
2017-08-10 18:31:19 +1000

    Prevent a request for master credentials
    When a session is requested from the keychain, the master credentials
    shouldn't be requested. Instead we should be prompting for access to the
    session keychain item.

--- vault/provider.go
+++ vault/provider.go
@@ -87,42 +87,12 @@ func NewVaultProvider(k keyring.Keyring, profile string, opts VaultOptions) (*Va
 	}, nil
 }
 
+// Retrieve returns credentials protected by a GetSessionToken. If there is an associated
+// role in the profile then AssumeRole is applied. The benefit of a session is that it doesn't
+// require MFA or a user prompt to access the keychain item, much like sudo.
 func (p *VaultProvider) Retrieve() (credentials.Value, error) {
-	creds, err := p.getMasterCreds()
-	if err != nil {
-		return credentials.Value{}, err
-	}
-
-	window := p.ExpiryWindow
-	if window == 0 {
-		window = time.Minute * 5
-	}
-
 	if p.NoSession {
-		if role, ok := p.profiles[p.profile]["role_arn"]; ok {
-			session, err := p.assumeRole(creds, role)
-			if err != nil {
-				return credentials.Value{}, err
-			}
-
-			log.Printf("Using role ****************%s, expires in %s",
-				(*session.AccessKeyId)[len(*session.AccessKeyId)-4:],
-				session.Expiration.Sub(time.Now()).String())
-
-			p.SetExpiration(*session.Expiration, window)
-			p.expires = *session.Expiration
-
-			value := credentials.Value{
-				AccessKeyID:     *session.AccessKeyId,
-				SecretAccessKey: *session.SecretAccessKey,
-				SessionToken:    *session.SessionToken,
-			}
-
-			return value, nil
-		}
-
-		// no role, exposes master credentials which don't expire
-		return creds, nil
+		return p.RetrieveWithoutSessionToken()
 	}
 
 	session, err := p.sessions.Retrieve(p.profile, p.SessionDuration)
@@ -131,6 +101,12 @@ func (p *VaultProvider) Retrieve() (credentials.Value, error) {
 			log.Println("Session not found in keyring")
 		} else {
 			log.Println(err)
+		}
+
+		// session lookup missed, we need to create a new one
+		creds, err := p.getMasterCreds()
+		if err != nil {
+			return credentials.Value{}, err
 		}
 
 		session, err = p.getSessionToken(&creds)
@@ -156,6 +132,11 @@ func (p *VaultProvider) Retrieve() (credentials.Value, error) {
 		log.Printf("Using role ****************%s (from session token), expires in %s",
 			(*session.AccessKeyId)[len(*session.AccessKeyId)-4:],
 			session.Expiration.Sub(time.Now()).String())
+	}
+
+	window := p.ExpiryWindow
+	if window == 0 {
+		window = time.Minute * 5
 	}
 
 	p.SetExpiration(*session.Expiration, window)

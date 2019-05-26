96ad4d7b077615ee2136f176327eddf3d59fac87
Stanisław Pitucha <viraptor@gmail.com>
2019-05-26 22:06:00 +1000

    Use single code instead of splitting
    `ykman oath code -s` gives a better interface which doesn't require
    splitting the output. It also doesn't have issues with line endings.
    Use that instead.

--- prompt/ykman.go
+++ prompt/ykman.go
@@ -2,15 +2,13 @@ package prompt
 
 import (
 	"os/exec"
-	"strings"
 )
 
 func YkmanPrompt(_prompt string, mfa_serial string) (string, error) {
-	cmd := exec.Command("ykman", "oath", "code", mfa_serial)
+	cmd := exec.Command("ykman", "oath", "code", "-s", mfa_serial)
 	out, err := cmd.Output()
 	if err != nil {
 		return "", err
 	}
-	parts := strings.Split(string(out), " ")
-	return parts[len(parts)-1], nil
+	return string(out[:len(out)-1]), nil
 }

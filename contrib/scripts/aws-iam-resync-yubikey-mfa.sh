41521c7168861dd20d9b3433d59b702a37acd7e9
Chris Mazanec <chris@pristin.io>
2021-02-21 14:46:17 +0100

    added timed wait between code generations, added resync script

--- contrib/scripts/aws-iam-resync-yubikey-mfa.sh
+++ contrib/scripts/aws-iam-resync-yubikey-mfa.sh
@@ -1,36 +1,13 @@
-#!/bin/sh
-# Adds a Yubikey TOTP device to IAM
+#!/usr/bin/env bash
 
 set -eu
-
-if [ -n "${AWS_SESSION_TOKEN:-}" ]; then
-  echo "aws-vault must be run without a STS session, please run it with the --no-session flag" >&2
-  exit 1
-fi
-
-cleanup()
-{
-  if [ -z "$OUTFILE" ]; then
-    rm "$OUTFILE"
-  fi
-}
-trap cleanup EXIT
 
 ACCOUNT_ARN=$(aws sts get-caller-identity --query Arn --output text)
 # Assume that the final portion of the ARN is the username
 # Works for ARNs like `users/<user>` and `users/engineers/<user>`
 USERNAME=$(echo "$ACCOUNT_ARN" | rev | cut -d/ -f1 | rev)
-
-OUTFILE=$(mktemp)
-SERIAL_NUMBER=$(aws iam create-virtual-mfa-device \
-  --virtual-mfa-device-name "$USERNAME" \
-  --bootstrap-method Base32StringSeed \
-  --outfile "$OUTFILE" \
-  --query VirtualMFADevice.SerialNumber \
-  --output text)
-
-ykman oath add -t "$SERIAL_NUMBER" < "$OUTFILE" 2> /dev/null
-rm "$OUTFILE"
+ACCOUNT_ID=$(echo "$ACCOUNT_ARN" | cut -d: -f5)
+SERIAL_NUMBER="arn:aws:iam::${ACCOUNT_ID}:mfa/${USERNAME}"
 
 CODE1=$(ykman oath code -s "$SERIAL_NUMBER")
 
@@ -48,8 +25,8 @@ sleep ${WAIT_SECONDS:-0}
 
 CODE2=$(ykman oath code -s "$SERIAL_NUMBER")
 
-aws iam enable-mfa-device \
-  --user-name "$USERNAME" \
-  --serial-number "$SERIAL_NUMBER" \
-  --authentication-code1 "$CODE1" \
-  --authentication-code2 "$CODE2"
+aws iam resync-mfa-device \
+    --user-name "$USERNAME" \
+    --serial-number "$SERIAL_NUMBER" \
+    --authentication-code1 "$CODE1" \
+    --authentication-code2 "$CODE2"

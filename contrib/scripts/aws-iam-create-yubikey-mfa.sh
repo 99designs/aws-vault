0bbe59037e28bb3a6be809cdea6abea0762a236e
Chris Mazanec <chris@pristin.io>
2021-02-22 07:38:26 +0100

    wrapped time generation logic into function

--- contrib/scripts/aws-iam-create-yubikey-mfa.sh
+++ contrib/scripts/aws-iam-create-yubikey-mfa.sh
@@ -16,6 +16,18 @@ cleanup()
 }
 trap cleanup EXIT
 
+waittime() {
+    # wait until next code can be generated
+    # if SECONDS are :00 or :30, generate right away
+    SECONDS=$(date +%S)
+    if (( ${SECONDS#0} >= 1 && ${SECONDS#0} <= 29 )); then
+        WAIT_SECONDS=$(( 30 - ${SECONDS#0} ))
+    elif (( ${SECONDS#0} >= 31 && ${SECONDS#0} <= 59 )); then
+        WAIT_SECONDS=$(( 60 - ${SECONDS#0} ))
+    fi
+    echo ${WAIT_SECONDS:-0}
+}
+
 ACCOUNT_ARN=$(aws sts get-caller-identity --query Arn --output text)
 # Assume that the final portion of the ARN is the username
 # Works for ARNs like `users/<user>` and `users/engineers/<user>`
@@ -34,17 +46,10 @@ rm "$OUTFILE"
 
 CODE1=$(ykman oath code -s "$SERIAL_NUMBER")
 
-# wait until next code can be generated
-# if SECONDS are :00 or :30, generate right away
-SECONDS=$(date +%S)
-if (( ${SECONDS#0} >= 1 && ${SECONDS#0} <= 29 )); then
-    WAIT_SECONDS=$(( 30 - ${SECONDS#0} ))
-elif (( ${SECONDS#0} >= 31 && ${SECONDS#0} <= 59 )); then
-    WAIT_SECONDS=$(( 60 - ${SECONDS#0} ))
-fi
+WAIT_TIME=$(waittime)
 
-echo "Waiting ${WAIT_SECONDS:-0}s before generating a second code" >&2
-sleep ${WAIT_SECONDS:-0}
+echo "Waiting ${WAIT_TIME}s before generating a second code" >&2
+sleep ${WAIT_TIME}
 
 CODE2=$(ykman oath code -s "$SERIAL_NUMBER")
 

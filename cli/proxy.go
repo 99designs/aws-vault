a6ac84b7c8d5c2de2d5f2dc735503f07325ce9c1
Michael Tibben <michael.tibben@99designs.com>
2022-01-18 12:07:27 +1100

    Use buffered channel for signals to fix go vet

--- cli/proxy.go
+++ cli/proxy.go
@@ -32,7 +32,7 @@ func ConfigureProxyCommand(app *kingpin.Application, a *AwsVault) {
 
 func handleSigTerm() {
 	// shutdown
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c

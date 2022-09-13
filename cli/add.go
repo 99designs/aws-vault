822d645ec0ee8547761294b794dfc27014d73edb
goldenbender <nlang@adobe.com>
2022-09-13 11:21:18 -0400

    updates & add auto-release

--- cli/add.go
+++ cli/add.go
@@ -27,7 +27,7 @@ func ConfigureAddCommand(app *kingpin.Application, a *AwsVault) {
 		Required().
 		StringVar(&input.ProfileName)
 
-	cmd.Flag("env", "Read the credentials from the environment (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY)").
+	cmd.Flag("env", "Read the credentials from the environment (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_SESSION_TOKEN)").
 		BoolVar(&input.FromEnv)
 
 	cmd.Flag("add-config", "Add a profile to ~/.aws/config if one doesn't exist").
@@ -50,7 +50,7 @@ func ConfigureAddCommand(app *kingpin.Application, a *AwsVault) {
 }
 
 func AddCommand(input AddCommandInput, keyring keyring.Keyring, awsConfigFile *vault.ConfigFile) error {
-	var accessKeyID, secretKey string
+	var accessKeyID, secretKey, sessionToken string
 
 	p, _ := awsConfigFile.ProfileSection(input.ProfileName)
 	if p.SourceProfile != "" {
@@ -65,6 +65,10 @@ func AddCommand(input AddCommandInput, keyring keyring.Keyring, awsConfigFile *v
 		if secretKey = os.Getenv("AWS_SECRET_ACCESS_KEY"); secretKey == "" {
 			return fmt.Errorf("Missing value for AWS_SECRET_ACCESS_KEY")
 		}
+		// Since AWS_SESSION_TOKEN is optional normally, don't return an error instead just send generic output
+		if sessionToken = os.Getenv("AWS_SESSION_TOKEN"); sessionToken == "" {
+			fmt.Printf("No optional AWS_SESSION_TOKEN variable found \n")
+		}
 	} else {
 		var err error
 		if accessKeyID, err = prompt.TerminalPrompt("Enter Access Key ID: "); err != nil {
@@ -73,9 +77,12 @@ func AddCommand(input AddCommandInput, keyring keyring.Keyring, awsConfigFile *v
 		if secretKey, err = prompt.TerminalSecretPrompt("Enter Secret Access Key: "); err != nil {
 			return err
 		}
+		if sessionToken, err = prompt.TerminalSecretPrompt("Enter Session Token: "); err != nil {
+			return err
+		}
 	}
 
-	creds := aws.Credentials{AccessKeyID: accessKeyID, SecretAccessKey: secretKey}
+	creds := aws.Credentials{AccessKeyID: accessKeyID, SecretAccessKey: secretKey, SessionToken: sessionToken}
 
 	ckr := &vault.CredentialKeyring{Keyring: keyring}
 	if err := ckr.Set(input.ProfileName, creds); err != nil {
